df9b46da19ebd1eab947e3c492d9f785c0f256ba
yushiuan9499 <yushiuan9499@gmail.com>
2025-12-09 19:19:11 +0800

    fix: mysql don't allow root login with phinx

--- config/config.example.php
+++ config/config.example.php
@@ -4,8 +4,8 @@ $_config = [];
 // ----------------------------  CONFIG DB  ----------------------------- //
 $_config['db']['query_string']  = 'mysql:host=127.0.0.1';
 $_config['db']['dbname'] = 'sky';
-$_config['db']['dbuser']        = 'root';
-$_config['db']['dbpassword']    = '';
+$_config['db']['dbuser']        = 'skyuser';
+$_config['db']['dbpassword']    = 'skypassword';
 $_config['db']['dbcharset']     = 'utf8';
 $_config['db']['tablepre']      = 'tojtest_';
 $_config['cookie']['namepre']   = 'tojtest';
