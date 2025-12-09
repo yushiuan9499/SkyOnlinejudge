9f6164f2f53b47b9f850236e682d9abbb7dfa5a1
yushiuan9499 <yushiuan9499@gmail.com>
2025-12-09 18:29:36 +0800

    fix: use localhost make mysql not to find host

--- config/config.example.php
+++ config/config.example.php
@@ -2,7 +2,7 @@
 
 $_config = [];
 // ----------------------------  CONFIG DB  ----------------------------- //
-$_config['db']['query_string']  = 'mysql:host=localhost';
+$_config['db']['query_string']  = 'mysql:host=127.0.0.1';
 $_config['db']['dbname'] = 'sky';
 $_config['db']['dbuser']        = 'root';
 $_config['db']['dbpassword']    = '';
